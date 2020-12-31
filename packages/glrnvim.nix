57fcbf7132d778de559afcfe6914b8148e78a2b5
ddrcode <ddrcode@gmail.com>
2020-12-31 14:05:04 +0000

    glrnvim version 1.1.2

--- packages/glrnvim.nix
+++ packages/glrnvim.nix
@@ -11,5 +11,5 @@ rustPlatform.buildRustPackage rec {
     sha256 = "17wbf4pm32kpi2nbphaq3vbhr13alxirwxhpjk7vgvhpnkww1kxn";
   };
 
-  cargoSha256 = "1b50kvl144njngnsr8k6kvnhsns97bcasss0b4v1lrkj01cpzwan";
+  cargoSha256 = "0bqvd52nf986vnhj0hjjmw0s53i1rin1ng6xprggwvfcq4y50hzm";
 }
