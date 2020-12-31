ad8237f1b300110d32cc1ddb0ff699872dfa4c93
ddrcode <ddrcode@gmail.com>
2020-12-31 13:58:30 +0000

    glrnvim version adjusted

--- packages/glrnvim.nix
+++ packages/glrnvim.nix
@@ -2,13 +2,13 @@
 
 rustPlatform.buildRustPackage rec {
   name = "glrnvim";
-  version = "v1.1.1";
+  version = "v1.1.2";
 
   src = fetchFromGitHub {
-    owner = "beeender";
+    owner = "ddrcode";
     repo = "glrnvim";
     rev = "${version}";
-    sha256 = "1nxxyj4nlvp0rm1vzpn1s6if77sb2k443a2g5x498qm8m3av4714";
+    sha256 = "17wbf4pm32kpi2nbphaq3vbhr13alxirwxhpjk7vgvhpnkww1kxn";
   };
 
   cargoSha256 = "1b50kvl144njngnsr8k6kvnhsns97bcasss0b4v1lrkj01cpzwan";
